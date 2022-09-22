19d589a10acf1c6f29aefb839fd495793d825034
Samuel Majerus <ladolego@gmail.com>
2022-09-22 11:44:41 -0700

    Adds iteration of Business Logic with the compile issues resolved

--- Scrabble/Models/Scorer.cs
+++ Scrabble/Models/Scorer.cs
@@ -14,28 +14,44 @@ namespace WordFilter.Models
 
     public int GetScore()
     {
-      char[] inputArray = InputWord.toStringArray().ToUpper();
+      string inputArray = InputWord.ToUpper();  //At this website, see the section "Access Strings": https://www.w3schools.com/cs/cs_strings.php
+      //Console.WriteLine(inputArray); //Confirm that 'inputArray' is formatted correctly.
       int score = 0;
-      string[] one = new string[10] {"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"};
-      string[] two = new string[2] {"D", "G"};
-      string[] three = new string[4] {"B", "C", "M", "P"};
-      string[] four = new string[5] {"F", "H", "V", "W", "Y"};
-      string[] five = new string[1] {"K"};
-      string[] six = new string[2] {"J", "X"};
-      for (int i=0; i<inputArray.Length; i++){
-        if(one.Contains(inputArray[i])){
+      // string[] one = new string[10] {"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"};
+      // string[] two = new string[2] {"D", "G"};
+      // string[] three = new string[4] {"B", "C", "M", "P"};
+      // string[] four = new string[5] {"F", "H", "V", "W", "Y"};
+      // string[] five = new string[1] {"K"};
+      // string[] six = new string[2] {"J", "X"};
+      
+      for (int i=0; i<inputArray.Length; i++)
+      {
+        if ((inputArray[i] == 'A') || (inputArray[i] == 'E') || (inputArray[i] == 'I') || (inputArray[i] == 'O') || (inputArray[i] == 'U') || (inputArray[i] == 'L') || (inputArray[i] == 'N') || (inputArray[i] == 'R') || (inputArray[i] == 'S') || (inputArray[i] == 'T'))
+        {
           score += 1;
-        } else if (two.Contains(inputArray[i])){
+        } 
+        else if ((inputArray[i] == 'D') || (inputArray[i] == 'G'))
+        {
           score += 2;
-        } else if (three.Contains(inputArray[i])){
+        } 
+        else if ((inputArray[i] == 'B') || (inputArray[i] == 'C') || (inputArray[i] == 'M') || (inputArray[i] == 'P'))
+        {
           score += 3;
-        } else if (four.Contains(inputArray[i])){
+        } 
+        else if ((inputArray[i] == 'F') || (inputArray[i] == 'H') || (inputArray[i] == 'V') || (inputArray[i] == 'W') || (inputArray[i] == 'Y'))
+        {
           score += 4;
-        } else if (five.Contains(inputArray[i])){
+        } 
+        else if (inputArray[i] == 'K')
+        {
           score += 5;
-        } else if (six.Contains(inputArray[i])){
+        } 
+        else if (inputArray[i] == 'J' || inputArray[i] == 'X')
+        {
           score += 8;
-        } else {  //For Q & Z
+        } 
+        else 
+        {  //For Q & Z
           score += 10;
         }
       }
